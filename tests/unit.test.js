2aa21331c3c7e8760968b879114ba2c80066c9e1
Alexander Ivanov <sashapop10@yandex.ru>
2023-10-11 20:03:45 +0300

    Schema from sample

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -2,7 +2,25 @@
 
 const test = require('node:test');
 const assert = require('node:assert');
+const schema = require('../lib/schema');
 
-test('First test', () => {
-  assert.strictEqual(1, 1);
+test('Generate schema from sample', () => {
+  const sample = {
+    lines: ['Pushkin streen', 'Kalatushkin home', 8],
+    zip: '123103',
+    city: 'Moscow',
+    country: 'Russia',
+  };
+  const expected = {
+    type: 'object',
+    properties: {
+      lines: { type: 'array', items: ['string', 'number'] },
+      zip: 'string',
+      city: 'string',
+      country: 'string',
+    },
+  };
+  const plan = schema.from(sample);
+  console.dir(plan, { depth: 4 });
+  assert.deepStrictEqual(plan, expected);
 });
