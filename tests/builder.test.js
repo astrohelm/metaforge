9a01cce1bfab56c753483b35118467360f9eaaae
Alexander Ivanov <sashapop10@yandex.ru>
2023-10-16 16:35:39 +0300

    v0.1.0

--- tests/builder.test.js
+++ tests/builder.test.js
@@ -1,31 +1,7 @@
 'use strict';
 
 const [test, assert] = [require('node:test'), require('node:assert')];
-const { Builder, prototypes } = require('../lib/types');
-const builder = new Builder(prototypes);
-
-test('Builder constructor', () => {
-  assert.strictEqual(typeof builder, 'object');
-  assert.strictEqual(typeof builder.build, 'function');
-  assert.strictEqual(typeof builder.typeOf, 'function');
-  assert.strictEqual(typeof builder.isShorthand, 'function');
-  assert.strictEqual(typeof builder.isRequired, 'function');
-  assert.strictEqual(typeof builder.conditions, 'function');
-  assert.strictEqual(typeof builder.builders, 'object');
-  assert.strictEqual(typeof builder.builders.array, 'function');
-  assert.strictEqual(typeof builder.builders.object, 'function');
-  assert.strictEqual(typeof builder.Error, 'function');
-});
-
-test('Builder error', () => {
-  const { Error } = builder;
-  const err = new Error({ path: 'Test', schema: {}, cause: 'Test', sampleType: 'string' });
-  assert.strictEqual(typeof err, 'object');
-  assert.strictEqual(JSON.stringify(err), '{"path":"Test","cause":"Test","count":1}');
-  assert.strictEqual('' + err, `[Test] => Test`);
-  assert.strictEqual(typeof err.add, 'function');
-  assert.strictEqual('' + err.add('New').add('Test'), `[Test] => Test, New, Test`);
-});
+const { Schema, prototypes } = require('../lib');
 
 test('Schema with errors & warnings', () => {
   const schema = {
@@ -48,13 +24,13 @@ test('Schema with errors & warnings', () => {
     hello: 'world',
     123: 'test', //? +2 Exoitic,  Missing field "z"
   };
-  const test = builder.build(schema);
-  assert.strictEqual(test.warnings.length, 1);
-  const { cause, message, path } = test.warnings[0];
+  const plan = new Schema(schema, { types: prototypes });
+  assert.strictEqual(plan.warnings.length, 1);
+  const { cause, message, path } = plan.warnings[0];
   assert.strictEqual(path, 'PREPROCESS');
   assert.strictEqual(cause, 'Shorthand usage with non-scalar schema');
   assert.strictEqual(message, '[PREPROCESS] => Shorthand usage with non-scalar schema');
-  const errors = test(sample);
+  const errors = plan.test(sample);
   assert.strictEqual(errors.length, 7);
 });
 
@@ -87,8 +63,44 @@ test('Schema without errors & warnings', () => {
     hello: 'world',
     z: 'test',
   };
-  const test = builder.build(schema);
-  assert.strictEqual(test.warnings.length, 0);
-  const errors = test(sample);
+  const plan = new Schema(schema, { types: prototypes });
+  assert.strictEqual(plan.warnings.length, 0);
+  const errors = plan.test(sample);
+  assert.strictEqual(errors.length, 0);
+});
+
+test('Schema with namespace', () => {
+  const schema = {
+    type: 'object',
+    properties: {
+      a: ['number', 'string'], //? anyof
+      b: { type: 'set', items: ['?string', 'any', 'unknown'], condition: 'allof' },
+      c: {
+        type: 'object',
+        properties: {
+          z: 'string',
+          //? Required shorthand don't work at array items
+          d: { type: 'array', items: ['?string', '?number'], condition: 'oneof' },
+        },
+      },
+      z: 'string',
+      z2: '?string', //? not required
+      z3: { type: 'string', required: false },
+    },
+    patternProperties: {
+      '^[a-z]+': 'string',
+    },
+  };
+  const sample = {
+    a: 'test',
+    b: new Set(['a', 'b', 'c']),
+    c: { z: 'string', d: [1, 'test'] },
+    hello: 'world',
+    z: 'test',
+  };
+  const PlanA = new Schema(schema, { types: prototypes });
+  const PlanB = new Schema({ type: 'PlanA' }, { types: prototypes, namespace: { PlanA } });
+  assert.strictEqual(PlanB.warnings.length + PlanA.warnings.length, 0);
+  const errors = PlanB.test(sample);
   assert.strictEqual(errors.length, 0);
 });
