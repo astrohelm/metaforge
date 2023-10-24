f70a369333c9c304dd5f16e758b20d2770240a83
sashapop10 <sashapop10@yandex.ru>
2023-10-24 12:04:56 +0300

    Array items as single type, string rules

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -13,3 +13,17 @@ test('Rules', () => {
   assert.strictEqual(schema.test('Test').length, 1); //? One rule
   assert.strictEqual(schema.test('Hello world').length, 0);
 });
+
+test('String type of rules', () => {
+  const lengthRule = sample => sample?.length > 5 && sample?.length < 100;
+  const mailRule = sample => sample?.includes('@');
+  const isGmail = sample => sample?.includes('gmail.com');
+  const rules = new Map().set('length', lengthRule).set('mail', mailRule);
+  const plan = { type: 'string', rules: ['length', 'mail', isGmail], required: false };
+  const schema = new Schema(plan, { rules });
+  assert.strictEqual(schema.warnings.length, 0);
+  assert.strictEqual(schema.test().length, 0);
+  console.log(schema.test('Alexander Ivanov'));
+  assert.strictEqual(schema.test('Alexander Ivanov').length, 2); //? Not a mail
+  assert.strictEqual(schema.test('somemail@gmail.com').length, 0);
+});
